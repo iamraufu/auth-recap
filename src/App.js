b504297f1dc7c4d7db4311f0a128ad36faa01110
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-03-19 12:10:42 +0600

    facebook, gmail authentication enabled

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { useState } from 'react';
+
+!firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
 
 function App() {
+  const [user, setUser] = useState({})
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+  const facebookProvider = new firebase.auth.FacebookAuthProvider();
+  const handleGoogleSignIn = () => {
+    firebase.auth()
+      .signInWithPopup(googleProvider)
+      .then((result) => {
+        const credential = result.credential;
+        const token = credential.accessToken;
+        const user = result.user;
+        console.log(token, user)
+        setUser(user)
+      }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.email;
+        const credential = error.credential;
+        console.log(errorCode, errorMessage, email, credential)
+      });
+  }
+  const handleFacebookSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(facebookProvider)
+      .then((result) => {
+        const credential = result.credential;
+        const user = result.user;
+        const accessToken = credential.accessToken;
+        console.log(accessToken, user)
+        setUser(user)
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.email;
+        const credential = error.credential;
+        console.log(errorCode, errorMessage, email, credential)
+      });
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={handleGoogleSignIn}>Sign In Using Google</button>
+        <br/>
+        <button onClick={handleFacebookSignIn}>Sign In Using Facebook</button>
+        <h1>Your Name: {user.displayName}</h1>
+        <h3>Email: {user.email}</h3>
+        <img src={user.photoURL} alt="" />
       </header>
     </div>
   );
